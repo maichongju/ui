e0a3654ae4804928ed30bef5fab4e13b555b6476
Chongju Mai <maichongju@hotmail.com>
2024-08-07 23:21:37 -0400

    feat: Added radio group cards

---
+++ apps/www/registry/default/example/radio-group-card-demo.tsx
@@ -0,0 +1,21 @@
+import { RadioGroup, RadioGroupCard } from "@/registry/default/ui/radio-group"
+
+export default function RadioGroupCardDemo() {
+  return (
+    <RadioGroup defaultValue="ground" className="flex space-x-2">
+      <RadioGroupCard value="ground">
+        <div className="p-6">
+          <p className="font-semibold">Ground Shipping</p>
+          <p className="text-sm">Deliver within 5-8 business day</p>
+        </div>
+      </RadioGroupCard>
+
+      <RadioGroupCard value="air">
+        <div className="p-6">
+          <p className="font-semibold">Air Shipping</p>
+          <p className="text-sm">Deliver within 3-5 business day</p>
+        </div>
+      </RadioGroupCard>
+    </RadioGroup>
+  )
+}
